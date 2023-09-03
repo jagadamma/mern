d499590796122ec9e179a777dd65c52436264531
root <root@ip-172-31-51-20.ec2.internal>
2023-09-03 22:44:08 +0000

    added all files

--- server/index.js
+++ server/index.js
@@ -14,11 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL= 'mongodb+srv://alanbinu:123123123@cluster0.hp31i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL= 'mongodb+srv://abi:abi@cluster0.lrxjklz.mongodb.net/mongo?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://54.237.26.94:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
